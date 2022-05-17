d2991989d199c95d4912267f9a1db71e30cdb9c7
Marco0259132 <100787396+Marco0259132@users.noreply.github.com>
2022-05-17 18:54:10 +0200

    Add files via upload

---
+++ code/flutter_application_1/lib/screens/login.dart
@@ -0,0 +1,279 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/homepage.dart';
+
+class Loginpage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: ListView(
+          children: [
+            SizedBox(height: screenHeight * .12),
+            const Text(
+              "Flexj,",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: screenHeight * .12,
+            ),
+            Text(
+              "To make your feel awesome, possibly",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.black.withOpacity(.6),
+              ),
+            ),
+            SizedBox(height: screenHeight * 0.05),
+            InputField(labelText: "Email"),
+            SizedBox(height: screenHeight * .025),
+            InputField(
+              labelText: "Password",
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {},
+                child: const Text(
+                  "Forgot Password?",
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: screenHeight * .075,
+            ),
+            FormButton(
+              text: "Log In",
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => homepage()),
+              ),
+            ),
+            SizedBox(
+              height: screenHeight * .15,
+            ),
+            TextButton(
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => const SimpleRegisterScreen(),
+                ),
+              ),
+              child: RichText(
+                text: const TextSpan(
+                  text: "I'm a new user, ",
+                  style: TextStyle(color: Colors.black),
+                  children: [
+                    TextSpan(
+                      text: "Sign Up",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SimpleRegisterScreen extends StatefulWidget {
+  /// Callback for when this form is submitted successfully. Parameters are (email, password)
+  final Function(String? email, String? password)? onSubmitted;
+
+  const SimpleRegisterScreen({this.onSubmitted, Key? key}) : super(key: key);
+
+  @override
+  _SimpleRegisterScreenState createState() => _SimpleRegisterScreenState();
+}
+
+class _SimpleRegisterScreenState extends State<SimpleRegisterScreen> {
+  late String email, password, confirmPassword;
+  String? emailError, passwordError;
+  Function(String? email, String? password)? get onSubmitted =>
+      widget.onSubmitted;
+
+  @override
+  void initState() {
+    super.initState();
+    email = "";
+    password = "";
+    confirmPassword = "";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: ListView(
+          children: [
+            SizedBox(height: screenHeight * .12),
+            const Text(
+              "Create Account,",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: screenHeight * .01),
+            Text(
+              "Sign up to get started!",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.black.withOpacity(.6),
+              ),
+            ),
+            SizedBox(height: screenHeight * .12),
+            InputField(
+              onChanged: (value) {
+                setState(() {
+                  email = value;
+                });
+              },
+              labelText: "Email",
+              errorText: emailError,
+              keyboardType: TextInputType.emailAddress,
+              textInputAction: TextInputAction.next,
+              autoFocus: true,
+            ),
+            SizedBox(height: screenHeight * .025),
+            InputField(
+              onChanged: (value) {
+                setState(() {
+                  password = value;
+                });
+              },
+              labelText: "Password",
+              errorText: passwordError,
+              obscureText: true,
+              textInputAction: TextInputAction.next,
+            ),
+            SizedBox(height: screenHeight * .025),
+            InputField(
+              onChanged: (value) {
+                setState(() {
+                  confirmPassword = value;
+                });
+              },
+              labelText: "Confirm Password",
+              errorText: passwordError,
+              obscureText: true,
+              textInputAction: TextInputAction.done,
+            ),
+            SizedBox(
+              height: screenHeight * .075,
+            ),
+            FormButton(
+              text: "Sign Up",
+            ),
+            SizedBox(
+              height: screenHeight * .125,
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: RichText(
+                text: const TextSpan(
+                  text: "I'm already a member, ",
+                  style: TextStyle(color: Colors.black),
+                  children: [
+                    TextSpan(
+                      text: "Sign In",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FormButton extends StatelessWidget {
+  final String text;
+  final Function? onPressed;
+  const FormButton({this.text = "", this.onPressed, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return ElevatedButton(
+      onPressed: onPressed as void Function()?,
+      child: Text(
+        text,
+        style: const TextStyle(fontSize: 16),
+      ),
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(vertical: screenHeight * .02),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+    );
+  }
+}
+
+class InputField extends StatelessWidget {
+  final String? labelText;
+  final Function(String)? onChanged;
+  final Function(String)? onSubmitted;
+  final String? errorText;
+  final TextInputType? keyboardType;
+  final TextInputAction? textInputAction;
+  final bool autoFocus;
+  final bool obscureText;
+  const InputField(
+      {this.labelText,
+      this.onChanged,
+      this.onSubmitted,
+      this.errorText,
+      this.keyboardType,
+      this.textInputAction,
+      this.autoFocus = false,
+      this.obscureText = false,
+      Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      autofocus: autoFocus,
+      onChanged: onChanged,
+      onSubmitted: onSubmitted,
+      keyboardType: keyboardType,
+      textInputAction: textInputAction,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        labelText: labelText,
+        errorText: errorText,
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+    );
+  }
+}
