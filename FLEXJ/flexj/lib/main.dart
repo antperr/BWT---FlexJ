14abeb9b8c72e579b66966d7bf84a05a64c04742
antperr <100787405+antperr@users.noreply.github.com>
2022-05-20 16:28:21 +0200

    Add files via upload

---
+++ FLEXJ/flexj/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flexj/screens/homepage.dart';
+import 'package:flexj/screens/pickvalue.dart';
+import 'package:flexj/screens/profilepage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+} //main
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //This specifies the app entrypoint
+      initialRoute: HomePage.route,
+      //This maps names to the set of routes within the app
+      routes: {
+         HomePage.route: (context) => HomePage(),
+        '/profile/': (context) => ProfilePage(), 
+        // '/message/': (context) => MessagePage(),
+        '/pickValue/': (context) => PickValuePage(),
+      },
+    );
+  } //build
+}//MyApp
---
+++ FLEXJ/flexj/lib/utils/strings.dart
@@ -0,0 +1,15 @@
+class Strings {
+
+  // Fitbit Client ID (replace XXX with your client ID)
+  static const String fitbitClientID = '238D4W';
+
+  // Fitbit Client Secret (replace XXX with your client secret)
+  static const String fitbitClientSecret = 'adaecf02664e3bccfbd3e4ee91da17c0';
+
+  /// Auth Uri (replace XXX with your Uri)
+  static const String fitbitRedirectUri = 'example://fitbit/auth';
+
+  /// Callback scheme (replace XXX with your callback scheme)
+  static const String fitbitCallbackScheme = 'example';
+
+}//Strings
