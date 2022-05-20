d95a0516d87f61e8c56ae2af0cfc47bc2e2eb3e4
antperr <100787405+antperr@users.noreply.github.com>
2022-05-20 16:37:36 +0200

    Add files via upload

---
+++ FLEXJ/flexj/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\BWT\\SOFTWARE\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\BWT\\FLEXJ\\flexj" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\BWT\\SOFTWARE\\flutter"
+  "PROJECT_DIR=D:\\BWT\\FLEXJ\\flexj"
+  "FLUTTER_ROOT=D:\\BWT\\SOFTWARE\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\BWT\\FLEXJ\\flexj\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\BWT\\FLEXJ\\flexj"
+  "FLUTTER_TARGET=D:\\BWT\\FLEXJ\\flexj\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\BWT\\FLEXJ\\flexj\\.dart_tool\\package_config.json"
+)
