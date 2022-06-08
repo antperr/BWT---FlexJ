b00ea0defadd9b88ed703857f18ff4fba2c3dbe2
Marco0259132 <100787396+Marco0259132@users.noreply.github.com>
2022-06-08 17:01:02 +0200

    Add files via upload

---
+++ code/flutter_application_1/lib/lib/screens/newhome.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
+
+class newhome extends StatefulWidget {
+  const newhome({Key? key}) : super(key: key);
+
+  @override
+  State<newhome> createState() => newhomeState();
+}
+
+class newhomeState extends State<newhome> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    dashboard(),
+    steps(),
+    hf(),
+    sleep(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //appBar: AppBar(
+      //  title: const Text(
+      //  "FlexJ",
+      //style: TextStyle(
+      //color: Colors.purple,
+      //  fontSize: 25,
+      //fontWeight: FontWeight.w600,
+      //),
+      //),
+      //centerTitle: true,
+      // backgroundColor: Colors.black,
+      // ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.directions_walk),
+            label: 'Activity',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.monitor_heart),
+            label: 'Heart Frequency',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bedtime),
+            label: 'Sleep',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.purple,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class steps extends StatelessWidget {
+  const steps({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    int obiettivoPassi = 8000;
+    int passiDay = 5000;
+    double counter = passiDay * 0.60;
+    double peso = 80;
+    double cal = 0.5 * peso * counter / 1000;
+    double passiPercent = passiDay / obiettivoPassi * 100;
+    if (passiPercent > 100) {
+      passiPercent = 100;
+    }
+    return Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.centerRight,
+                end: Alignment.centerLeft,
+                colors: [
+              Colors.purple,
+              Colors.red,
+            ])),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircularStepProgressIndicator(
+                totalSteps: obiettivoPassi,
+                currentStep: passiDay,
+                stepSize: 10,
+                selectedColor: Colors.red,
+                unselectedColor: Colors.black,
+                padding: 0,
+                width: 250,
+                height: 250,
+                selectedStepSize: 15,
+              ),
+              Text(
+                "$passiPercent %",
+                style: TextStyle(fontStyle: FontStyle.italic, fontSize: 80),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.local_fire_department),
+                      Text("Calories= $cal kcal")
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [Icon(Icons.directions_walk), Text("$counter m")],
+                  )
+                ],
+              )
+            ]));
+  }
+}
+
+class hf extends StatelessWidget {
+  const hf({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xffC4DFCB),
+      child: Center(
+        child: Text(
+          "Page Number 3",
+          style: TextStyle(
+            color: Colors.green[900],
+            fontSize: 45,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class dashboard extends StatelessWidget {
+  const dashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: [
+            Colors.black,
+            Colors.purple,
+          ])),
+      child: ListView(
+        children: [
+          Image.network(
+            "https://cdn3.iconfinder.com/data/icons/vector-icons-6/96/256-512.png",
+            scale: 3,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class sleep extends StatelessWidget {
+  const sleep({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.centerRight,
+              end: Alignment.centerLeft,
+              colors: [
+            Colors.indigo.shade900,
+            Colors.indigo,
+          ])),
+    );
+  }
+}
