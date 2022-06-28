49772e3a26e2d74305ed5dac8cf91b1cd20a0076
antperr <antonio.perrella@studenti.unipd.it>
2022-06-28 18:58:38 +0200

    2

--- FLEXJ/flexj/lib/screens/home.dart
+++ FLEXJ/flexj/lib/screens/home.dart
@@ -1,23 +1,29 @@
+import 'package:flexj/utils/fitData.dart';
 import 'package:flutter/material.dart';
 
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
 
   static const route = '/home';
   static const routeDisplayName = 'HomePage';
 
+  final FetchedFitData userData = FetchedFitData();
+
 
   @override
   Widget build(BuildContext context) {
+        print('${HomePage.routeDisplayName} built');
+
     return Scaffold(
         appBar: AppBar(
           centerTitle: true,
-          title: const Text(
-            'Dashboard',
-          ),
+          title: const Text(HomePage.routeDisplayName),
           backgroundColor: Colors.black,
+
         ),
+
+  
         body: Column(
           children: [
             ElevatedButton(onPressed: () {}, child: Text("to dashboard"))
