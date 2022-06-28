7fae66da843282890c007764479e59d7594712b7
antperr <antonio.perrella@studenti.unipd.it>
2022-06-28 17:36:01 +0200

    Update fitData.dart

--- FLEXJ/flexj/lib/utils/fitData.dart
+++ FLEXJ/flexj/lib/utils/fitData.dart
@@ -3,56 +3,6 @@ import 'package:fitbitter/fitbitter.dart';
 import 'package:flexj/utils/strings.dart';
 import 'package:flutter/cupertino.dart';
 
-FitbitActivityTimeseriesDataManager
-                    fitbitActivityTimeseriesDataManager =
-                    FitbitActivityTimeseriesDataManager(
-                  clientID: Strings.fitbitClientID,
-                  clientSecret: Strings.fitbitClientSecret,
-                  type: 'steps',
-                );
-
-
-
-
-
-
-
-Future<double?> fetchSteps() async{
-
-  double? steps=0;
-
-  String? userId = await FitbitConnector.authorize(
-                    context: ,
-                    clientID: Strings.fitbitClientID,
-                    clientSecret: Strings.fitbitClientSecret,
-                    redirectUri: Strings.fitbitRedirectUri,
-                    callbackUrlScheme: Strings.fitbitCallbackScheme);
-                    
-                //Instantiate a proper data manager
-  FitbitActivityTimeseriesDataManager
-        fitbitActivityTimeseriesDataManager =
-        FitbitActivityTimeseriesDataManager(
-            clientID: '<OAuth 2.0 Client ID>',
-            clientSecret: '<Client Secret>',
-            type: 'steps', 
-        );
-
-                //Fetch data
-  final stepsData = await fitbitActivityTimeseriesDataManager.fetch(
-  FitbitActivityTimeseriesAPIURL.dayWithResource(
-      date: DateTime.now().subtract(Duration(days: 1)),
-      userID: userId,
-      resource: fitbitActivityTimeseriesDataManager.type,
-      )
-  ) as List<FitbitActivityTimeseriesData>;
-
-  
-
-  steps=stepsData.last.value;
-
-  return steps;
-
-}
 
 
 
@@ -73,6 +23,44 @@ class FetchedFitData extends ChangeNotifier{
   FetchedFitData.withHeight({this.altezza});
   FetchedFitData.withGoal({this.goal});
   FetchedFitData.withGender({this.genere});
+
+    Future<double?> fetchSteps() async{
+
+      double? steps=0;
+
+      String? userId = await FitbitConnector.authorize(
+                        context: ,
+                        clientID: Strings.fitbitClientID,
+                        clientSecret: Strings.fitbitClientSecret,
+                        redirectUri: Strings.fitbitRedirectUri,
+                        callbackUrlScheme: Strings.fitbitCallbackScheme);
+                        
+                    //Instantiate a proper data manager
+      FitbitActivityTimeseriesDataManager
+            fitbitActivityTimeseriesDataManager =
+            FitbitActivityTimeseriesDataManager(
+                clientID: '<OAuth 2.0 Client ID>',
+                clientSecret: '<Client Secret>',
+                type: 'steps', 
+            );
+
+                    //Fetch data
+      final stepsData = await fitbitActivityTimeseriesDataManager.fetch(
+                FitbitActivityTimeseriesAPIURL.dayWithResource(
+                    date: DateTime.now().subtract(Duration(days: 1)),
+                    userID: userId,
+                    resource: fitbitActivityTimeseriesDataManager.type,
+                    )
+      ) as List<FitbitActivityTimeseriesData>;
+
+      
+
+      steps=stepsData.last.value;
+
+      return steps;
+    }
+
+
 
 }
 
