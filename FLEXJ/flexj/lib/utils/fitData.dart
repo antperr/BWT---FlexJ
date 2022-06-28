6c45edd6c0ca93b4006edf0cc20475e8a00a221b
antperr <antonio.perrella@studenti.unipd.it>
2022-06-28 17:28:36 +0200

    fitData

--- FLEXJ/flexj/lib/utils/fitData.dart
+++ FLEXJ/flexj/lib/utils/fitData.dart
@@ -14,26 +14,13 @@ FitbitActivityTimeseriesDataManager
 
 
 
-class FetchedFitData extends ChangeNotifier{
-
-  double? passi;
-  double? bpm;
-  List<FitbitActivityTimeseriesData>? hr;
-  
-  FetchedFitData();
-
-  FetchedFitData.withstep({this.passi});
-  FetchedFitData.withhr({this.bpm});
-  FetchedFitData.withtimehr({this.hr});
-
-}
 
 
-Future<FetchedFitData> fetchin() async{
 
-  FetchedFitData fitData;
+Future<double?> fetchSteps() async{
 
-  
+  double? steps=0;
+
   String? userId = await FitbitConnector.authorize(
                     context: ,
                     clientID: Strings.fitbitClientID,
@@ -42,27 +29,57 @@ Future<FetchedFitData> fetchin() async{
                     callbackUrlScheme: Strings.fitbitCallbackScheme);
                     
                 //Instantiate a proper data manager
-  FitbitActivityTimeseriesDataManager fitbitActivityTimeseriesDataManager = 
-                    FitbitActivityTimeseriesDataManager(
-                                clientID: Strings.fitbitClientID,
-                                clientSecret: Strings.fitbitClientSecret,
-                                type: 'steps',
-                    );
+  FitbitActivityTimeseriesDataManager
+        fitbitActivityTimeseriesDataManager =
+        FitbitActivityTimeseriesDataManager(
+            clientID: '<OAuth 2.0 Client ID>',
+            clientSecret: '<Client Secret>',
+            type: 'steps', 
+        );
 
                 //Fetch data
-                final hrData = await fitbitActivityTimeseriesDataManager.fetch(
-                                          FitbitActivityTimeseriesAPIURL.dayWithResource(
-                                            date: DateTime.now().subtract(Duration(days: 1)),
-                                            userID: userId,
-                                            resource: fitbitActivityTimeseriesDataManager.type,
-                                          )
-                                        ) as List<FitbitActivityTimeseriesData>;
+  final stepsData = await fitbitActivityTimeseriesDataManager.fetch(
+  FitbitActivityTimeseriesAPIURL.dayWithResource(
+      date: DateTime.now().subtract(Duration(days: 1)),
+      userID: userId,
+      resource: fitbitActivityTimeseriesDataManager.type,
+      )
+  ) as List<FitbitActivityTimeseriesData>;
+
+  
+
+  steps=stepsData.last.value;
+
+  return steps;
+
+}
 
 
-    fitData=FetchedFitData.withstep(passi: 10);
+
+
+class FetchedFitData extends ChangeNotifier{
+
+  double? passi;
+  // double? bpm;
+  double? peso;
+  double? altezza;
+  double? goal;
+  bool? genere;
+
+  FetchedFitData();
+
+  FetchedFitData.withstep({this.passi});
+  FetchedFitData.withWeight({this.peso});
+  FetchedFitData.withHeight({this.altezza});
+  FetchedFitData.withGoal({this.goal});
+  FetchedFitData.withGender({this.genere});
+
+}
+
+
+
+
+/*  fitData=FetchedFitData.withstep(passi: 10);
     fitData=FetchedFitData.withhr(bpm: 100);
     fitData=FetchedFitData.withtimehr(hr: hrData);
-
-
-  return fitData;
-}
+ */
